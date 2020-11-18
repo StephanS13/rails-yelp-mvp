2ea9bf0d56858c9968a4f1bbbb5ba9dd9806f0fc
Stephane Simcic <simcic.stephane@gmail.com>
2020-11-18 16:51:04 +0100

    ex2 nearly complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+
+McDo = Restaurant.create!(name: "McDonalds", address: "Paris", category: "american")
+PetitFrancois = Restaurant.create!(name: "Le Petit Francois", address: "Nantes", category: "french")
+Odissey = Restaurant.create!(name: "Odissey", address: "Paris", category: "french")
+Tacos = Restaurant.create!(name: "Tacos", address: "Marseille", category: "mexican")
+Pizza = Restaurant.create!(name: "Pizza", address: "Paris", category: "italian")
